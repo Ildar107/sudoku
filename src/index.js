0094042a99f24b670b43e37b94364ad980999853
ildar107 <ildar107wk@gmail.com>
2019-10-16 17:57:33 +0300

    feat: add part of decision

--- src/index.js
+++ src/index.js
@@ -1,80 +1,115 @@
 module.exports = function solveSudoku(matrix) {
-  return getSolve(matrix);
+  var result = getSolve(matrix)
+  console.log(result);
+  return result;
+}
+const values = [1,2,3,4,5,6,7,8,9];
+
+function getSolve(matrix, allVariations = null) {
+  if(allVariations === null)
+    allVariations = getAllVariation(matrix);
+
+  if(matrix.findIndex(x=> x.findIndex(y => y === 0) >= 0) < 0)
+    return matrix;
+
+  for(let cell of allVariations)
+    for(let num of cell.variations)
+    {
+      if(checkAllRules(matrix, cell.row, cell.column, num))
+      {
+        let newMatrix = matrix.slice(0);
+        newMatrix[cell.row][cell.column] = num;
+        newMatrix = getSolve(newMatrix, allVariations.slice(0));
+        if(newMatrix !== null)
+          return newMatrix;
+      }
+    }
+  return null;
 }
 
-function getSolve(matrix) {
+function checkAllRules(matrix, i, j, num)
+{
+  return checkHorisontalValue(matrix[i], num) && checkVerticalValue(matrix, j, num)
+    && checkCubeValue(matrix, i, j, num);
+}
 
-  while(true)
+function getUpdatedVariation(matrix, allVariations)
+{
+  let newAllVariations = [];
+  for(let cell of allVariations)
   {
-    let nextValueChange = {row : 0, column : 0, variations : [] };
-    for(let i = 0; i < matrix.length; i++)
-      for(let j = 0; j < matrix[i].length; j++)
-      {
-        let variationArr = [];
-        let values = [1,2,3,4,5,6,7,8,9];
-        if(matrix[i][j] !== 0) continue;
-        values.forEach(value => {
-          if(checkHorisontalValue(matrix[i], value) && checkVerticalValue(matrix, j, value)
-              && checkCubeValue(matrix, i, j, value))
-            variationArr.push(value);
-        })
-        if(variationArr.length === 1)
-        {
-            matrix[i][j] = variationArr.pop();
-            continue;
-        }
-        if((nextValueChange.variations.length === 0 || nextValueChange.variations.length > variationArr.length)
-              && variationArr.length > 0)
-        {
-          nextValueChange.variations = variationArr.sort();
-          nextValueChange.column = j;
-          nextValueChange.row = i;
-        }  
-      }
-    
-    if(matrix.findIndex(x=> x.findIndex(y => y === 0) >= 0) < 0)
-      return matrix;
-
-    if(nextValueChange.variations.length === 0)
-      return null;
-      
-    if(nextValueChange.variations.length > 0)
+    let newVariations = []
+    for(let num of values)
     {
-      let newMatrix = matrix.slice(0, 9);
-      newMatrix[nextValueChange.row][nextValueChange.column] = nextValueChange.variations.shift();
-      newMatrix = getSolve(newMatrix);
-      if(newMatrix !== null)
-       return newMatrix;
+      if(checkAllRules(matrix, cell.row, cell.column, num))
+            newVariations.push(num);
+    }
+    if(newVariations.length === 1)
+    {
+      matrix[cell.row][cell.column] = newVariations.pop();
+      isNotChanged = false;
+      continue;
+    }
+    if(newVariations.length > 0)
+    {
+      cell.variations = newVariations.sort();
+      newAllVariations.push(cell);
     }
   }
+  allVariations = newAllVariations.sort((x,y) => x.variations.length < y.variations.length ? -1 
+    : x.variations.length > y.variations.length ? 1 : 0);
 }
 
+function getAllVariation(matrix)
+{
+  var allVariations = [];
+  for(let i = 0; i < matrix.length; i++)
+    for(let j = 0; j < matrix[i].length; j++)
+    {
+      if(matrix[i][j] !== 0) continue;
+      let variationForCell = {row: i, column: j, variations: []};
+      for(let num of values) 
+      {
+        if(checkAllRules(matrix, i, j, num))
+          variationForCell.variations.push(num);
+      }
+      if(variationForCell.variations.length === 1)
+      {
+          matrix[i][j] = variationForCell.variations.pop();
+          continue;
+      }
+      variationForCell.variations = variationForCell.variations.sort();
+      allVariations.push(variationForCell);
+    }
+  allVariations = allVariations.sort((x,y) => x.variations.length < y.variations.length ? -1 
+    : x.variations.length > y.variations.length ? 1 : 0);
+  return allVariations;
+}
 
-function checkHorisontalValue(horizontalArr, value)
+function checkHorisontalValue(horizontalArr, num)
 {
   for(let i = 0; i < horizontalArr.length; i++)
-    if(horizontalArr[i] === value)
+    if(horizontalArr[i] === num)
       return false;
   return true;
 }
 
-
-function checkVerticalValue(matrix, j, value)
+function checkVerticalValue(matrix, j, num)
 {
   for(let i = 0; i < matrix.length; i++)
-    if(matrix[i][j] === value)
+    if(matrix[i][j] === num)
       return false;
   return true;
 }
 
-function checkCubeValue(matrix, i, j, value)
+function checkCubeValue(matrix, i, j, num)
 {
   let cubeRowStart = i < 3 ? 0 : i < 6 ? 3 : 6;  
   let cubeColumnStart = j < 3 ? 0 : j < 6 ? 3 : 6;  
   for(let a = cubeRowStart; a < (cubeRowStart + 3); a++)
     for(let b = cubeColumnStart; b < (cubeColumnStart+ 3); b++)
     {
-      if(matrix[a][b] === value)
+      if(matrix[a][b] === num)
         return false;
     }
   return true;
